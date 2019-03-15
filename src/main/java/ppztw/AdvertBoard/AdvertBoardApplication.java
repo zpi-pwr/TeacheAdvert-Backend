890776ee818c660cbb65da30dde009bec4db035c
mateuszp <mpftp10@gmail.com>
2019-03-15 20:11:34 +0100

    enabled app config

--- src/main/java/ppztw/AdvertBoard/AdvertBoardApplication.java
+++ src/main/java/ppztw/AdvertBoard/AdvertBoardApplication.java
@@ -2,8 +2,11 @@ package ppztw.AdvertBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ppztw.AdvertBoard.Config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class AdvertBoardApplication {
 
 	public static void main(String[] args) {
