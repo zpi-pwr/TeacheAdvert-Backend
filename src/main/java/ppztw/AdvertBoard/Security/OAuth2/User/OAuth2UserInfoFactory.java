68a58092a89bedae69b83787fd208ae709b1aa4d
mateuszp <mpftp10@gmail.com>
2019-03-15 20:06:10 +0100

    defined oauth2 user info services

--- src/main/java/ppztw/AdvertBoard/Security/OAuth2/User/OAuth2UserInfoFactory.java
+++ src/main/java/ppztw/AdvertBoard/Security/OAuth2/User/OAuth2UserInfoFactory.java
@@ -1,4 +1,19 @@
 package ppztw.AdvertBoard.Security.OAuth2.User;
 
+import ppztw.AdvertBoard.Exception.OAuth2AuthenticationProcessingException;
+import ppztw.AdvertBoard.Model.AuthProvider;
+
+import java.util.Map;
+
 public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
+            return new FacebookOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }
+    }
 }
