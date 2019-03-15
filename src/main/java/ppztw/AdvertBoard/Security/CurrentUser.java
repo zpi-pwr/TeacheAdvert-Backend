777806b935b13b4fb1d6eb20ec25e61fb6b8cb54
mateuszp <mpftp10@gmail.com>
2019-03-15 19:55:24 +0100

    defined security services

--- src/main/java/ppztw/AdvertBoard/Security/CurrentUser.java
+++ src/main/java/ppztw/AdvertBoard/Security/CurrentUser.java
@@ -1,4 +1,13 @@
 package ppztw.AdvertBoard.Security;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
 public @interface CurrentUser {
+
 }
