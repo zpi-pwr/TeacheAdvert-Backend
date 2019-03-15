d39b74d7c06b33f456e1b61b39ab1c4a781caa1e
mateuszp <mpftp10@gmail.com>
2019-03-15 16:30:53 +0100

    Initial commit

---
+++ src/main/java/ppztw/AdvertBoard/AdvertBoardApplication.java
@@ -0,0 +1,13 @@
+package ppztw.AdvertBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertBoardApplication.class, args);
+	}
+
+}
