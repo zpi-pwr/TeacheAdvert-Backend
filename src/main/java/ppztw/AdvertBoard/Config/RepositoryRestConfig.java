9c1de1ab75b8864512b49589ba190821cb8a44ef
mateuszp <mpftp10@gmail.com>
2019-03-24 12:14:36 +0100

    defined repository rest config

---
+++ src/main/java/ppztw/AdvertBoard/Config/RepositoryRestConfig.java
@@ -0,0 +1,16 @@
+package ppztw.AdvertBoard.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.setPageParamName("p")
+                .setLimitParamName("l")
+                .setSortParamName("q");
+    }
+}
