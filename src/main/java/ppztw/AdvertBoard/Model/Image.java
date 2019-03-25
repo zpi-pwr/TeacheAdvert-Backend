d8ccc3b089e13ec80cff6b1dc85f3f4e16afa55f
mateuszp <mpftp10@gmail.com>
2019-03-25 13:29:08 +0100

    added getters and setters and set nullable to false

--- src/main/java/ppztw/AdvertBoard/Model/Image.java
+++ src/main/java/ppztw/AdvertBoard/Model/Image.java
@@ -1,21 +1,27 @@
 package ppztw.AdvertBoard.Model;
 
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "images")
 @NoArgsConstructor
+@Getter
+@Setter
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String filename;
 
     @Lob
+    @Column(nullable = false)
     private byte[] pic;
 
     public Image(String filename, byte[] pic) {
