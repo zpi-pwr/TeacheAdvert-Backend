1ec19ed7cc624d3f4ee9fd86a325cd26e79384fd
mateuszp <mpftp10@gmail.com>
2019-04-06 17:51:17 +0200

    added category info to category view

--- src/main/java/ppztw/AdvertBoard/View/CategoryView.java
+++ src/main/java/ppztw/AdvertBoard/View/CategoryView.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ppztw.AdvertBoard.Model.Category;
+import ppztw.AdvertBoard.Model.CategoryInfo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ public class CategoryView {
     Long id;
     String name;
     List<CategoryView> subcategories;
+    List<CategoryInfoView> infoList;
 
     public CategoryView(Category category) {
         this.id = category.getId();
@@ -23,5 +25,8 @@ public class CategoryView {
         for (Category subcategory : category.getSubcategories()) {
             subcategories.add(new CategoryView(subcategory));
         }
+        infoList = new ArrayList<>();
+        for (CategoryInfo info : category.getInfoList())
+            infoList.add(new CategoryInfoView(info));
     }
 }
