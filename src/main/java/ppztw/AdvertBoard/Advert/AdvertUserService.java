3d6c918722169150ae855df73b2b100869748d25
mateuszp <mpftp10@gmail.com>
2019-04-07 14:29:53 +0200

    made findAdvert public once again, moved finding user to findAdvert class

--- src/main/java/ppztw/AdvertBoard/Advert/AdvertUserService.java
+++ src/main/java/ppztw/AdvertBoard/Advert/AdvertUserService.java
@@ -41,7 +41,7 @@ public class AdvertUserService {
     @Autowired
     private AdvertRepository advertRepository;
 
-    private Optional<Advert> findAdvert(Long userId, Long id) {
+    public Optional<Advert> findAdvert(Long userId, Long id) {
         User user = userRepository.findById(userId).orElseThrow(() ->
                 new ResourceNotFoundException("User", "id", userId));
         List<Advert> adverts = user.getAdverts();
