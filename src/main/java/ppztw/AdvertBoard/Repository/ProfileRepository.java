b291bbb124d00dc9a1a4c7a62d6bdca0f99650eb
mateuszp <mpftp10@gmail.com>
2019-05-04 00:49:51 +0200

    defined findAll and findAllByVisibleNameLike methods, returning pages

--- src/main/java/ppztw/AdvertBoard/Repository/ProfileRepository.java
+++ src/main/java/ppztw/AdvertBoard/Repository/ProfileRepository.java
@@ -1,6 +1,10 @@
 package ppztw.AdvertBoard.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ppztw.AdvertBoard.Model.Profile;
 
 import java.util.Optional;
@@ -8,4 +12,9 @@ import java.util.Optional;
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
     Optional<Profile> findByUserId(Long userId);
+
+    Page<Profile> findAll(Pageable pageable);
+
+    @Query("SELECT p FROM #{#entityName} p WHERE p.visibleName LIKE CONCAT('%',:substr,'%')")
+    Page<Profile> findAllByVisibleNameLike(@Param("substr") String nameContains, Pageable pageable);
 }
