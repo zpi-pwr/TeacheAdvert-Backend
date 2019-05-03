8f7ebe13e6f855a86341a959402f2726ea0a0c85
mateuszp <mpftp10@gmail.com>
2019-05-04 00:49:12 +0200

    defined findAllByCategoryIdIn and findAllByCategoryIdInAndTitleLike methods

--- src/main/java/ppztw/AdvertBoard/Repository/Advert/AdvertRepository.java
+++ src/main/java/ppztw/AdvertBoard/Repository/Advert/AdvertRepository.java
@@ -1,10 +1,24 @@
 package ppztw.AdvertBoard.Repository.Advert;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ppztw.AdvertBoard.Model.Advert.Advert;
 
-@Repository
-public interface AdvertRepository extends PagingAndSortingRepository<Advert, Long> {
+import java.util.List;
 
+@Repository
+public interface AdvertRepository extends JpaRepository<Advert, Long> {
+
+    @Query("SELECT a FROM #{#entityName} a WHERE a.category.id IN :ids")
+    Page<Advert> findAllByCategoryIdIn(@Param("ids") List<Long> categoryIdList, Pageable pageable);
+
+    @Query("SELECT a FROM #{#entityName} a " +
+            "WHERE a.category.id IN :ids AND a.title LIKE CONCAT('%',:substr,'%')")
+    Page<Advert> findAllByCategoryIdInAndTitleLike(@Param("ids") List<Long> categoryIdList,
+                                                   @Param("substr") String titleContains,
+                                                   Pageable pageable);
 }
