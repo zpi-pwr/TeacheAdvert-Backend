f7fbaae657c35032538dbd22080462b1ef50a42e
mateuszp <mpftp10@gmail.com>
2019-05-04 00:48:27 +0200

    removed unused methods

--- src/main/java/ppztw/AdvertBoard/Repository/Advert/CategoryRepository.java
+++ src/main/java/ppztw/AdvertBoard/Repository/Advert/CategoryRepository.java
@@ -4,12 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ppztw.AdvertBoard.Model.Advert.Category;
 
-import java.util.Optional;
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Category> findByCategoryName(String category_name);
-
-    Boolean existsByCategoryName(String category_name);
 }
