9578a4a3bf98d2e197af5540dde6f70af57a610f
mateuszp <mpftp10@gmail.com>
2019-05-04 21:09:42 +0200

    defined Report class

---
+++ src/main/java/ppztw/AdvertBoard/Model/Report.java
@@ -0,0 +1,26 @@
+package ppztw.AdvertBoard.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+import ppztw.AdvertBoard.Model.Advert.Advert;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @OneToOne
+    User reportingUser;
+
+    @OneToOne
+    Advert reportedAdvert;
+
+    String comment;
+
+}
