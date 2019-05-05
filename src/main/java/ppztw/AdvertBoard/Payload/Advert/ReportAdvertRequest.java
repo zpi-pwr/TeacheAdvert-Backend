1a03e94bcb30cff9aedb6e1062efc0a2a45b888a
mateuszp <mpftp10@gmail.com>
2019-05-05 15:40:24 +0200

    added NotNull annotations to fields

--- src/main/java/ppztw/AdvertBoard/Payload/Advert/ReportAdvertRequest.java
+++ src/main/java/ppztw/AdvertBoard/Payload/Advert/ReportAdvertRequest.java
@@ -4,10 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class ReportAdvertRequest {
+
+    @NotNull
     Long advertId;
+
+    @NotNull
     String comment;
 }
